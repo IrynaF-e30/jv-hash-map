833f49953f2b942a2028fa7ac64a02772b3ffd0d
Iryna Finenko <irynafinenko@gmail.com>
2025-08-12 21:42:58 +0300

    fixed bugs

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,13 +1,84 @@
 package core.basesyntax;
 
-import org.w3c.dom.Node;
+import java.util.Objects;
 
-public class MyHashMap<K, V> {
+public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
 
+    private Node<K, V>[] table;
+    private int size;
+
+    @SuppressWarnings("unchecked")
     public MyHashMap() {
-        Node[] table =  new Node[DEFAULT_CAPACITY];
-        int size = 0;
-      }
+        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
+        size = 0;
     }
+
+    @Override
+    public void put(K key, V value) {
+        if ((float) size / table.length >= LOAD_FACTOR) {
+            resize();
+        }
+        int index = getIndex(key);
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                current.value = value;
+                return;
+            }
+            current = current.next;
+        }
+        table[index] = new Node<>(key, value, table[index]);
+        size++;
+    }
+
+    @Override
+    public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private int getIndex(K key) {
+        return (key == null) ? 0 : (key.hashCode() & 0x7fffffff) % table.length;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        Node<K, V>[] oldTable = table;
+        table = (Node<K, V>[]) new Node[oldTable.length * 2];
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
+
+
--- src/main/java/core/basesyntax/MyMap.java
+++ src/main/java/core/basesyntax/MyMap.java
@@ -1,83 +1,8 @@
 package core.basesyntax;
-import java.util.Objects;
 
-public class MyHashMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 16;
-    private static final float LOAD_FACTOR = 0.75f;
-
-    private Node<K, V>[] table;
-    private int size;
-
-    @SuppressWarnings("unchecked")
-    public MyHashMap() {
-        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
-        size = 0;
-    }
-
-    public void put(K key, V value) {
-        if ((float) size / table.length >= LOAD_FACTOR) {
-            resize();
-        }
-        int index = getIndex(key);
-        Node<K, V> current = table[index];
-
-        while (current != null) {
-            if (Objects.equals(current.key, key)) {
-                current.value = value; // оновлюємо існуюче значення
-                return;
-            }
-            current = current.next;
-        }
-
-        table[index] = new Node<>(key, value, table[index]);
-        size++;
-    }
-
-    public V getValue(K key) {
-        int index = getIndex(key);
-        Node<K, V> current = table[index];
-
-        while (current != null) {
-            if (Objects.equals(current.key, key)) {
-                return current.value;
-            }
-            current = current.next;
-        }
-
-        return null;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    private int getIndex(K key) {
-        return (key == null) ? 0 : (key.hashCode() & 0x7fffffff) % table.length;
-    }
-
-    @SuppressWarnings("unchecked")
-    private void resize() {
-        Node<K, V>[] oldTable = table;
-        table = (Node<K, V>[]) new Node[oldTable.length * 2];
-        size = 0; // заново перерахуємо при rehash
-
-        for (Node<K, V> node : oldTable) {
-            while (node != null) {
-                put(node.key, node.value);
-                node = node.next;
-            }
-        }
-    }
-
-    private static class Node<K, V> {
-        K key;
-        V value;
-        Node<K, V> next;
-
-        Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
+public interface MyMap<K, V> {
+    void put(K key, V value);
+    V getValue(K key);
+    int getSize();
 }
+
